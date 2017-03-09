5ebcac747477c7813292254eee691f7fe4214d5d
Jonathan Moody <jonmoody12@gmail.com>
2017-03-08 21:43:19 -0500

    Created shell for game code file

--- nes-tutorial.asm
+++ nes-tutorial.asm
@@ -2,3 +2,28 @@
 ;
 ; Author: Jonathan Moody
 ; Github: https://github.com/jonmoody
+
+  .inesprg 1    ; Defines the number of 16kb PRG banks
+  .ineschr 1    ; Defines the number of 8kb CHR banks
+  .inesmap 0    ; Defines the NES mapper
+  .inesmir 1    ; Defines VRAM mirroring of banks
+
+  .bank 0
+  .org $C000
+
+RESET:
+
+InfiniteLoop:
+  JMP InfiniteLoop
+
+NMI:
+  RTI
+
+  .bank 1
+  .org $FFFA
+  .dw NMI
+  .dw RESET
+  .dw 0
+
+  .bank 2
+  .org $0000
