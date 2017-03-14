94c18abf5003bf0816ca25f366d7714e1d64b883
Jonathan Moody <jonmoody12@gmail.com>
2017-03-13 21:42:22 -0400

    Loading a background

--- nes-tutorial.asm
+++ nes-tutorial.asm
@@ -8,18 +8,66 @@
   .inesmap 0    ; Defines the NES mapper
   .inesmir 1    ; Defines VRAM mirroring of banks
 
+  .rsset $0000
+pointerBackgroundLowByte  .rs 1
+pointerBackgroundHighByte .rs 1
+
   .bank 0
   .org $C000
 
 RESET:
+  JSR LoadBackground
+
+  LDA #%10000000   ; Enable NMI, sprites and background on table 0
+  STA $2000
+  LDA #%00011110   ; Enable sprites, enable backgrounds
+  STA $2001
+  LDA #$00         ; No background scrolling
+  STA $2006
+  STA $2006
+  STA $2005
+  STA $2005
 
 InfiniteLoop:
   JMP InfiniteLoop
+
+LoadBackground:
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$00
+  STA $2006
+
+  LDA #LOW(background)
+  STA pointerBackgroundLowByte
+  LDA #HIGH(background)
+  STA pointerBackgroundHighByte
+
+  LDX #$00
+  LDY #$00
+.Loop:
+  LDA [pointerBackgroundLowByte], y
+  STA $2007
+
+  INY
+  CPY #$00
+  BNE .Loop
+
+  INC pointerBackgroundHighByte
+  INX
+  CPX #$04
+  BNE .Loop
+  RTS
 
 NMI:
   RTI
 
   .bank 1
+  .org $E000
+
+background:
+  .include "graphics/background.asm"
+
   .org $FFFA
   .dw NMI
   .dw RESET
@@ -27,3 +75,4 @@ NMI:
 
   .bank 2
   .org $0000
+  .incbin "graphics.chr"
